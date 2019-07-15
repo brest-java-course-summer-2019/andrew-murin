54da5daedefce6d8deba99496eff9d7bf6b7d2a7
Andrew Murin <alze.andrey.1997@mail.ru>
2019-07-15 14:05:17 +0300

    Made new branch with travis.yml

---
+++ model/ticket-train/src/main/java/com.epam.brest.summer.courses2019.model/Ticket.java
@@ -0,0 +1,4 @@
+package com.epam.brest.summer.courses2019.model;
+
+public class Ticket {
+}
---
+++ model/ticket-train/src/test/test/java/com/epam/brest/summer/courses2019/model/TicketTest.java
@@ -0,0 +1,35 @@
+package test.java.com.epam.brest.summer.courses2019.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.math.BigDecimal;
+
+
+public class TicketTest {
+
+    com.epam.brest.summer.courses2019.model.Ticket ticket = new Ticket();
+
+    @Test
+    public void getTicketId() {
+        ticket.setTicketId(5);
+        Assert.assertEquals(ticket.getTicketId().intValue(),5);
+    }
+
+    @Test
+    public void getCost() {
+        ticket.setCost(new BigDecimal("30"));
+        Assert.assertTrue(ticket.getCost().compareTo(new BigDecimal("30")) == 0);
+    }
+
+    @Test
+    public void getDataTime() {
+        ticket.setDataTime("10/07/2019");
+        Assert.assertTrue(ticket.getDataTime().equals("10/07/2019"));
+    }
+
+    @Test
+    public void getDirection() {
+        ticket.setDirection("Warsaw");
+        Assert.assertTrue(ticket.getTicketId().equals("Warsaw"));
+    }
+}
