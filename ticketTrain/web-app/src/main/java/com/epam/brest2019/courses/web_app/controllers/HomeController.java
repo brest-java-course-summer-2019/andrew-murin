c57a50fb30be4d64600a212ffc69e833673e5518
Andrew Murin <alze.andrey.1997@mail.ru>
2019-10-06 14:25:31 +0300

    Comment HomeController and Test on his

--- ticketTrain/web-app/src/main/java/com/epam/brest2019/courses/web_app/controllers/HomeController.java
+++ ticketTrain/web-app/src/main/java/com/epam/brest2019/courses/web_app/controllers/HomeController.java
@@ -1,25 +1,25 @@
-package com.epam.brest2019.courses.web_app.controllers;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class HomeController {
-
-    /**
-     *Logger
-     */
-    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
-
-    /**
-     * Default page
-     * @return index-view
-     */
-    @GetMapping("/index")
-    public String home() {
-        LOGGER.debug("Index controller");
-        return "index";
-    }
-}
+//package com.epam.brest2019.courses.web_app.controllers;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class HomeController {
+//
+//    /**
+//     *Logger
+//     */
+//    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+//
+//    /**
+//     * Default page
+//     * @return index-view
+//     */
+//    @GetMapping("/index")
+//    public String home() {
+//        LOGGER.debug("Index controller");
+//        return "index";
+//    }
+//}
