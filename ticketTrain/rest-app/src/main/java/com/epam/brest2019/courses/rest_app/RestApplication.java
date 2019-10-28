23563ff53804b904b1161ad5938d772e7c6e7293
Andrei Muryn <Andrei_Muryn@epam.com>
2019-10-28 17:29:56 +0300

    Add profiles to project in rest-app and delete for now test in dao

--- ticketTrain/rest-app/src/main/java/com/epam/brest2019/courses/rest_app/RestApplication.java
+++ ticketTrain/rest-app/src/main/java/com/epam/brest2019/courses/rest_app/RestApplication.java
@@ -2,7 +2,6 @@ package com.epam.brest2019.courses.rest_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(scanBasePackages = {"com.epam.brest2019.courses.*"})
 public class RestApplication {
