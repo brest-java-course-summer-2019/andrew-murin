599f35f2a9b9e859dd812a469c6bfee58924f12d
Andrei Muryn <Andrei_Muryn@epam.com>
2019-12-09 14:04:15 +0300

    comment paymentValidator

--- ticketTrain/web-app/src/test/java/com/epam/brest2019/courses/web_app/validators/PaymentValidatorTest.java
+++ ticketTrain/web-app/src/test/java/com/epam/brest2019/courses/web_app/validators/PaymentValidatorTest.java
@@ -1,30 +1,30 @@
-//package com.epam.brest2019.courses.web_app.validators;
-//
-//import com.epam.brest2019.courses.model.Payment;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//import org.springframework.validation.BeanPropertyBindingResult;
-//import org.springframework.validation.BindingResult;
-//
-//import java.time.LocalDate;
-//
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//public class PaymentValidatorTest {
-//
-//    private Payment payment;
-//
-//    private PaymentValidator paymentValidator = new PaymentValidator();
-//
-//    private BindingResult result;
-//
-//    @BeforeEach
-//    void setup() {
-//        payment = Mockito.mock(Payment.class);
-//        result = new BeanPropertyBindingResult(payment, "payment");
-//    }
-//
+package com.epam.brest2019.courses.web_app.validators;
+
+        import com.epam.brest2019.courses.model.Payment;
+        import org.junit.jupiter.api.BeforeEach;
+        import org.junit.jupiter.api.Test;
+        import org.mockito.Mockito;
+        import org.springframework.validation.BeanPropertyBindingResult;
+        import org.springframework.validation.BindingResult;
+
+        import java.time.LocalDate;
+
+        import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PaymentValidatorTest {
+
+    private Payment payment;
+
+    private PaymentValidator paymentValidator = new PaymentValidator();
+
+    private BindingResult result;
+
+    @BeforeEach
+    void setup() {
+        payment = Mockito.mock(Payment.class);
+        result = new BeanPropertyBindingResult(payment, "payment");
+    }
+
 //    @Test
 //    void shouldRejectEmptyPaymentTicketId() {
 //        Mockito.when(payment.getPaymentId()).thenReturn(null);
@@ -51,4 +51,4 @@
 //
 //        assertTrue(result.hasErrors());
 //    }
-//}
+}
