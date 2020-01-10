562b1ee204288b635bd1f58d6f7d9104d999912b
Andrei Muryn <Andrei_Muryn@epam.com>
2020-01-10 16:49:53 +0300

    nearly work test

--- ticketTrain/rest-app/src/test/java/com/epam/brest2019/courses/rest_app/wiremock/PaymentRestControllerWireMockTest.java
+++ ticketTrain/rest-app/src/test/java/com/epam/brest2019/courses/rest_app/wiremock/PaymentRestControllerWireMockTest.java
@@ -1,6 +1,8 @@
 package com.epam.brest2019.courses.rest_app.wiremock;
 
-import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+import com.epam.brest2019.courses.model.Payment;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.mysql.cj.util.TestUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,11 +10,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import wiremock.org.apache.commons.io.FileUtils;
 import wiremock.org.apache.http.HttpStatus;
 
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,13 +38,51 @@ public class PaymentRestControllerWireMockTest {
     private ApplicationContext applicationContext;
 
     @Rule
-    public WireMockClassRule wireMockClassRule = new WireMockClassRule(wireMockConfig().port(9999));
+    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().port(8089));
 
     @Test
-    public void whenRequestGetPayments() {
-        stubFor(get(urlEqualTo("/payments"))
+    public void whenRequestGetPayments() throws IOException {
+        ResponseEntity payments = restTemplate.getForEntity("http://localhost:8088/payments", String.class);
+
+//        String body = payments.getBody().toString();
+
+        wireMockRule.stubFor(get(urlMatching("/payments"))
             .willReturn(aResponse()
-                    .withStatus(HttpStatus.SC_OK)));
+                    .withStatus(HttpStatus.SC_OK)
+                    .withBody(String.valueOf(payments.getBody()))));
+
+
+
+        assertEquals(payments.getStatusCodeValue(), 200);
+        assertNotNull(payments.getBody());
+        assertEquals(String.valueOf(payments.getBody()), FileUtils.readFileToString(FileUtils.toFile(TestUtils.class.getClassLoader().getResource("mapping/payments.json")), String.valueOf(StandardCharsets.UTF_8)));
+
     }
+
+    private String getJSON(String path) throws IOException {
+        Resource resource = applicationContext.getResource("classpath:" + path);
+        return new String(Files.readAllBytes(resource.getFile().toPath()));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
