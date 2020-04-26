09e1fe3c00ed6cf066c6faeaa725ee1c425f00fc
Andrewka-Java <alze.andrey.1997@mail.ru>
2020-04-26 18:06:54 +0300

    rename objects on citySoap, ticketSoap, paymentSoap

--- ticketTrain/soap-app/src/main/java/com/epam/brest2019/courses/soap/SoapApplication.java
+++ ticketTrain/soap-app/src/main/java/com/epam/brest2019/courses/soap/SoapApplication.java
@@ -3,7 +3,7 @@ package com.epam.brest2019.courses.soap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.epam.brest2019.courses.*")
 public class SoapApplication {
 
     public static void main(String[] args) {
--- ticketTrain/soap-app/src/main/java/com/epam/brest2019/courses/soap/converter/CityConverter.java
+++ ticketTrain/soap-app/src/main/java/com/epam/brest2019/courses/soap/converter/CityConverter.java
@@ -1,4 +1,12 @@
 package com.epam.brest2019.courses.soap.converter;
 
+
+import com.epam.brest2019.courses.model.City;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CityConverter {
+
+
+
 }
