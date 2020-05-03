2512fb9cbab28684860e21eddda71dbd99018e6d
Andrewka-Java <alze.andrey.1997@mail.ru>
2020-05-03 18:26:16 +0300

    added tests on ticket in soap-app

--- ticketTrain/soap-app/src/test/java/com/epam/brest2019/courses/soap/TicketEndpointTest.java
+++ ticketTrain/soap-app/src/test/java/com/epam/brest2019/courses/soap/TicketEndpointTest.java
@@ -1,68 +1,111 @@
-//package com.epam.brest2019.courses.soap;
-//
-//import com.epam.brest2019.courses.model.Ticket;
-//import com.epam.brest2019.courses.service.TicketService;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.context.ResourceLoaderAware;
-//import org.springframework.core.io.Resource;
-//import org.springframework.core.io.ResourceLoader;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.ws.test.server.MockWebServiceClient;
-//
-//import java.io.IOException;
-//
-//import static org.junit.jupiter.api.Assertions.assertNotEquals;
-//import static org.springframework.ws.test.server.RequestCreators.withSoapEnvelope;
-//import static org.springframework.ws.test.server.ResponseMatchers.soapEnvelope;
-//
-//
-//@ExtendWith(SpringExtension.class)
-//@ContextConfiguration(classes = SoapApplication.class)
-//public class TicketEndpointTest implements ResourceLoaderAware {
-//
-//    @Autowired
-//    private ApplicationContext context;
-//    @Autowired
-//    private TicketService ticketService;
-//    private MockWebServiceClient mockClient;
-//    private ResourceLoader resourceLoader;
-//
-//
-//    @Override
-//    public void setResourceLoader(ResourceLoader resourceLoader) {
-//        this.resourceLoader = resourceLoader;
-//    }
-//
-//    @BeforeEach
-//    void createClient() {
-//        mockClient = MockWebServiceClient.createClient(context);
-//    }
-//
-//    @Test
-////    @Disabled
-//    void getAllTicket() throws IOException {
-//        Resource request = resourceLoader.getResource("/requests/ticket/getAllTicketRequest.xml");
-//        Resource response = resourceLoader.getResource("/responses/ticket/getAllTicketResponse.xml");
-//
-//        mockClient.sendRequest(withSoapEnvelope(request)).andExpect(soapEnvelope(response));
-//    }
-//
-//    @Test
-//    void getUpdateTicket() throws IOException {
-//        Resource request = resourceLoader.getResource("/requests/ticket/getUpdateTicketRequest.xml");
-//
-//        Ticket ticket = ticketService.findById(1);
-//
-//        mockClient.sendRequest(withSoapEnvelope(request));
-//
-//        Ticket updatedTicket = ticketService.findById(1);
-//
-//        assertNotEquals(ticket, updatedTicket, "Objects are the same");
-//    }
-//}
+package com.epam.brest2019.courses.soap;
+
+import com.epam.brest2019.courses.model.Ticket;
+import com.epam.brest2019.courses.service.TicketService;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.ws.test.server.MockWebServiceClient;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.springframework.ws.test.server.RequestCreators.withSoapEnvelope;
+import static org.springframework.ws.test.server.ResponseMatchers.soapEnvelope;
+
+
+@SpringBootTest(classes = SoapApplication.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TicketEndpointTest implements ResourceLoaderAware {
+
+    @Autowired
+    private ApplicationContext context;
+    @Autowired
+    private TicketService ticketService;
+    private MockWebServiceClient mockClient;
+    private ResourceLoader resourceLoader;
+
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
+    @BeforeEach
+    void createClient() {
+        mockClient = MockWebServiceClient.createClient(context);
+    }
+
+    @Test
+    void getAllTicket() throws IOException {
+        Resource request = resourceLoader.getResource("classpath:/requests/ticket/getAllTicketRequest.xml");
+        Resource response = resourceLoader.getResource("classpath:/responses/ticket/getAllTicketResponse.xml");
+
+        mockClient.sendRequest(withSoapEnvelope(request)).andExpect(soapEnvelope(response));
+    }
+
+    @Test
+    void getAllTicketWithDirection() throws IOException {
+        Resource request = resourceLoader
+                .getResource("classpath:/requests/ticket/getAllTicketWithDirectionRequest.xml");
+        Resource response = resourceLoader
+                .getResource("classpath:/responses/ticket/getAllTicketWithDirectionResponse.xml");
+
+        mockClient.sendRequest(withSoapEnvelope(request)).andExpect(soapEnvelope(response));
+    }
+
+    @Test
+    @Order(1)
+    void getSearchTicket() throws IOException {
+        Resource request = resourceLoader
+                .getResource("classpath:/requests/ticket/getSearchTicketByDateAndDirectionRequest.xml");
+        Resource response = resourceLoader
+                .getResource("classpath:/responses/ticket/getSearchTicketByDateAndDirectionResponse.xml");
+
+        mockClient.sendRequest(withSoapEnvelope(request)).andExpect(soapEnvelope(response));
+    }
+
+    @Test
+    void getUpdateTicket() throws IOException {
+        Resource request = resourceLoader.getResource("classpath:/requests/ticket/getUpdateTicketRequest.xml");
+
+        Ticket ticket = ticketService.findById(1);
+
+        mockClient.sendRequest(withSoapEnvelope(request));
+
+        Ticket updatedTicket = ticketService.findById(1);
+
+        assertNotEquals(ticket, updatedTicket, "Tickets are the same");
+    }
+
+    @Test
+    void getAddTicket() throws IOException {
+        Resource request = resourceLoader.getResource("classpath:/requests/ticket/getAddTicketRequest.xml");
+
+        int sizeBefore = ticketService.findAll().size();
+
+        mockClient.sendRequest(withSoapEnvelope(request));
+
+        int sizeAfter = ticketService.findAll().size();
+
+        assertEquals(sizeBefore + 1, sizeAfter, "Sizes are different");
+    }
+
+    @Test
+    void getDeleteTicket() throws IOException {
+        Resource request = resourceLoader.getResource("classpath:/requests/ticket/getDeleteTicketRequest.xml");
+
+        int sizeBefore = ticketService.findAll().size();
+
+        mockClient.sendRequest(withSoapEnvelope(request));
+
+        int sizeAfter = ticketService.findAll().size();
+
+        assertEquals(sizeBefore, sizeAfter + 1, "Sizes are the same");
+    }
+}
