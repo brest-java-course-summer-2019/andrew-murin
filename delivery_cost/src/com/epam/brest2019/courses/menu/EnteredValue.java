80b632c760c259df6f48a083ed53f256b54fd746
Andrewka-Java <alze.andrey.1997@mail.ru>
2020-07-20 12:29:25 +0300

    were deleted unnecessary folders

--- delivery_cost/src/com/epam/brest2019/courses/menu/EnteredValue.java
+++
@@ -1,6 +0,0 @@
-package com.epam.brest2019.courses.menu;
-
-public interface EnteredValue {
-    enum Types {EXIT, INCORRECT, VALUE}
-    Types getType();
-}
--- delivery_cost/src/com/epam/brest2019/courses/menu/IncorrectValue.java
+++
@@ -1,8 +0,0 @@
-package com.epam.brest2019.courses.menu;
-
-public class IncorrectValue implements EnteredValue {
-    @Override
-    public Types getType(){
-        return Types.INCORRECT;
-    }
-}
